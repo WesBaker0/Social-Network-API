caad04bfecabd362b5ec0a26e369bcc01789ca17
Weston Baker <westonbaker0@gmail.com>
2023-06-05 23:03:05 -0400

    Finished-Routes

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes'));
+const routes = require('./routes');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+app.use('/api', routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: false
 });
 
 mongoose.set('debug', true);
--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('dateformat');
 
 const ReactionSchema = new Schema(
   {
